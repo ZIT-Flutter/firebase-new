2e8bef90190bfca4451dd075d04d214c21a2c982
ZIT-Flutter <120543444+ZIT-Flutter@users.noreply.github.com>
2024-05-15 16:55:38 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/mongodb_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mongodb_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/homepage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () async {}, child: const Text('Add Date to DB')),
+      ),
+    );
+  }
+}
